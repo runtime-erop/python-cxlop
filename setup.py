c4e2e2dea1d80b9f12453e76074bc4c61be0c62d
runtime-erop <egor.srvc.letovo@gmail.com>
2025-02-18 16:47:25 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="cxlop",
+    version="0.1.0",
+    author="runtime-erop",
+    description="desc",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/runtime-erop",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
